d58ff168bd96563ba96d57329a4234faf66e2a28
Ina <cati_ciobanu@yahoo.com>
2023-02-24 19:31:27 +0000

    added the pseudocode provided by Dan on the live Slack channel and the style css file

--- index.js
+++ index.js
@@ -13,3 +13,69 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+// TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) { // or whatever parameters are required
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+}
+
+module.exports = Employee;
+
+// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(name, id, email, github) { // and whatever else is necessary
+        super(name, id, email);
+        this.github = github;
+    }
+}
+
+module.exports = Engineer;
+
+// Manager Questions
+inquirer.prompt([{
+    //manager questions
+}]).then(response => {
+    // populate manager info
+    // promptForNexEmployee ()
+})
+
+const promptForNextEmployee = () => {
+    inquirer.prompt([{
+        // choice of 3
+    }]).then(response => {
+        // if engineer
+        //    promptForEngineer
+        // else if intern
+        //    promptForIntern
+        // else
+        //    use the functionality from page-template to generate the team
+    })
+}
+// Questions for Engineer
+const promptForEngineer = () => {
+    inquirer.prompt([{
+        //engineer questions
+    }]).then(response => {
+        // add new engineer to employees array
+        // promptForNextEmployee
+    })
+}
+
+//Questions for intern
+const promptForIntern = () => {
+    inquirer.prompt([{
+        //intern questions
+    }]).then(response => {
+        // add new intern to employees array
+        // promptForNextEmployee
+    })
+}
+
+const buildPage = () => {
+// render(myArrayOfTeamMembers)
+}
